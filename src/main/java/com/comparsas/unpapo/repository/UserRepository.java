5ee0dc5b33dac1b75bf9d01f70fc00cc4e302f43
lucas.marinho <lucas.marinho@parnamirim.rn.gov.br>
2024-09-26 14:33:28 -0300

    feat: repositorio do usuario

--- src/main/java/com/comparsas/unpapo/repository/UserRepository.java
+++ src/main/java/com/comparsas/unpapo/repository/UserRepository.java
@@ -3,5 +3,9 @@ package com.comparsas.unpapo.repository;
 import com.comparsas.unpapo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByEmail(String email);
 }
