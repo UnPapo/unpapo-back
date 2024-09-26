8fc33d1c34b46094535a45e5c9729ebfb20782d4
lucas.marinho <lucas.marinho@parnamirim.rn.gov.br>
2024-09-26 14:33:21 -0300

    feat: controller do usuario

---
+++ src/main/java/com/comparsas/unpapo/controller/UserController.java
@@ -0,0 +1,34 @@
+package com.comparsas.unpapo.controller;
+
+import com.comparsas.unpapo.entity.User;
+import com.comparsas.unpapo.service.UserService;
+import com.comparsas.unpapo.utils.models.ApiResponse;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/user")
+public class UserController {
+
+    private final UserService userService;
+
+    @PostMapping()
+    public ResponseEntity<ApiResponse<User>> createUser(@RequestBody User user) throws Exception {
+        ApiResponse<User> userApiResponse = userService.createUser(user);
+        return ResponseEntity.status(userApiResponse.getStatus()).body(userApiResponse);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<ApiResponse<User>> updateUser(@PathVariable Long id, @RequestBody User user) {
+        ApiResponse<User> userApiResponse = userService.updateUser(id, user);
+        return ResponseEntity.status(userApiResponse.getStatus()).body(userApiResponse);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ApiResponse<User>> getUserById(@PathVariable Long id) {
+        ApiResponse<User> userApiResponse = userService.getUserById(id);
+        return ResponseEntity.status(userApiResponse.getStatus()).body(userApiResponse);
+    }
+}
