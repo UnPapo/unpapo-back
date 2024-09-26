3acd17627c518cec07e776b6b417f996c4d18bd4
lucas.marinho <lucas.marinho@parnamirim.rn.gov.br>
2024-09-26 14:32:45 -0300

    feat: modelo padrão de respotqa da API

---
+++ src/main/java/com/comparsas/unpapo/utils/models/ApiResponse.java
@@ -0,0 +1,53 @@
+package com.comparsas.unpapo.utils.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@SuppressWarnings("unused")
+public class ApiResponse<T> {
+
+    private int status = HttpStatus.OK.value();
+    private String message = "";
+    private T data = null;
+
+    public ApiResponse<T> of(T data) {
+        this.data = data;
+        return this;
+    }
+
+    public ApiResponse<T> of(String message) {
+        this.message = message;
+        return this;
+    }
+
+    public ApiResponse<T> of(HttpStatus httpStatus, String message) {
+        this.status = httpStatus.value();
+        this.message = message;
+        return this;
+    }
+
+    public ApiResponse<T> of(HttpStatus httpStatus, T data) {
+        this.status = httpStatus.value();
+        this.data = data;
+        return this;
+    }
+
+    public ApiResponse<T> of(HttpStatus httpStatus, String message, T data) {
+        this.status = httpStatus.value();
+        this.message = message;
+        this.data = data;
+        return this;
+    }
+
+    public ApiResponse<T> of(String message, T data) {
+        this.message = message;
+        this.data = data;
+        return this;
+    }
+
+}
